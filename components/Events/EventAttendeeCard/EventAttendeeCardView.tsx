cebcf08971daf4254a17eccfcb38b9c2cd86b90e
Benoist Bouteiller <b@bouteiller.contact>
2022-09-10 20:13:22 +0200

    style(attendees): add avatar

--- components/Events/EventAttendeeCard/EventAttendeeCardView.tsx
+++ components/Events/EventAttendeeCard/EventAttendeeCardView.tsx
@@ -1,4 +1,5 @@
 import { AttendeeInterface } from '../../../types/attendee.interface'
+import Avatar from '../../_ui/Avatar/Avatar'
 import classes from './EventAttendeeCard.module.css'
 
 
@@ -10,6 +11,9 @@ readonly type?: string
 export default function EventAttendeeCardView({user,type}:Props){
 
   return <div className={classes.container}>
+            <div className={classes.avatar}>
+          <Avatar src={user.avatar} />
+        </div>
     <div className={classes.name}>{user.name}</div>
     {type && <div className={classes.type}>{type}</div>}
   </div>
