3a0ea91bdbfffeed9498a52c8a6c39648aad12d2
Benoist Bouteiller <b@bouteiller.contact>
2022-10-04 14:36:07 +0200

    fix(pusher): fix for bad browser

--- components/_hooks/usePush.tsx
+++ components/_hooks/usePush.tsx
@@ -6,33 +6,33 @@ export default function usePush() {
   const [isConnected, setIsConnected] = useState<boolean>(false),
     { data: session } = useSession();
 
+  async function addPush(beamsClient: any) {
+    try {
+      const pushClient = await beamsClient.start();
+      if (pushClient) console.log("Vous pouvez recevoir des notifs");
+    } catch (e) {
+      console.log("Votre navigateur bloque les notif push", e);
+      return;
+    } finally {
+      setIsConnected(true);
+    }
+    session.user.profiles.forEach((profile: string) => {
+      beamsClient.addDeviceInterest("profile-" + profile);
+    });
+    beamsClient.addDeviceInterest("league-public");
+    if (session?.user?.league)
+      beamsClient.addDeviceInterest("league-" + session.user.league.id);
+    //private msg
+    beamsClient.addDeviceInterest("user-" + session.user._id);
+  }
   useEffect(() => {
-    if (session && session.user) {
+    if (session && session.user && !isConnected) {
       const beamsClient = new PusherPushNotifications.Client({
         instanceId: process.env.NEXT_PUBLIC_PUSHER_BEAMS,
       });
-
-      if (beamsClient) {
-        beamsClient
-          .start()
-          .then((beamsClient:any) => beamsClient.getDeviceId())
-          .then((_deviceId) =>
-            console.log("Vous pouvez recevoir des notifs")
-          )
-          // Sub to all profiles and leagues push
-          .then(() => {
-            session.user.profiles.forEach((profile: string) => {
-              beamsClient.addDeviceInterest('profile-'+profile);
-            });
-            beamsClient.addDeviceInterest('league-public');
-            if(session?.user?.league)
-            beamsClient.addDeviceInterest('league-' + session.user.league.id);
-            //private msg
-            beamsClient.addDeviceInterest("user-" + session.user._id)
-          })
-          .then(() => setIsConnected(true))
-          .catch(console.error);
-      }
+      
+      if (beamsClient) addPush(beamsClient);
+   
     }
   }, [session]);
 
