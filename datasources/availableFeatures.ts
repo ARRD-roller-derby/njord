fc5e5ba19f39b3a8178c0c5e264ee7e7bd4f55c8
Kazerlelutin <b@bouteiller.contact>
2022-10-08 16:53:54 +0200

    feat(*): change price

--- datasources/availableFeatures.ts
+++ datasources/availableFeatures.ts
@@ -3,7 +3,7 @@ import { AvailableFeatureInterface } from "../types/feature.interface";
 export const availableFeatures:Array<AvailableFeatureInterface> = [
   {
     name:'attendees_for_day',
-    cost:500,
+    cost:250,
     description:"Vous pouvez voir les participants aux événéments",
     exp:{
       delay:1,
