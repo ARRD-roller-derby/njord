c6e52084c0420432717c464d452ea3ba8fb7070d
Kazerlelutin <b@bouteiller.contact>
2023-01-05 00:19:22 +0100

    fix(mock): add key for user

--- __MOCK__/simpleUser.ts
+++ __MOCK__/simpleUser.ts
@@ -1,17 +1,19 @@
 import { UserInterface } from "../types/User.interface";
 
-export const simpleUser:UserInterface = {
+export const simpleUser: UserInterface = {
   _id: 'simple_user_id',
-  email:'simple@user.fr',
-  name:'simple',
-  lastname:'user',
-  derbyName:'mega shark',
-  numRoster:'01',
-  msp:false,
-  mst:true,
-  profiles:[],
-  league:{
-    id:'my_league_id',
-    shortName:'my league'
+  email: 'simple@user.fr',
+  name: 'simple',
+  lastname: 'user',
+  derbyName: 'mega shark',
+  numRoster: '01',
+  msp: false,
+  mst: true,
+  profiles: [],
+  dailyContestAvgTime: 13,
+  dailyContestAvgAccuracy: 50,
+  league: {
+    id: 'my_league_id',
+    shortName: 'my league'
   }
 }
