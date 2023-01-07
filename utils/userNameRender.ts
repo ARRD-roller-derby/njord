e4159c83a2ff79713d73454633ba5e3fac93c3c9
Kazerlelutin <b@bouteiller.contact>
2023-01-07 18:16:32 +0100

    fix(name): escape

--- utils/userNameRender.ts
+++ utils/userNameRender.ts
@@ -1,6 +1,7 @@
 import { UserInterface } from '../types/User.interface'
+import validator from 'validator'
 
 export default function userNameRender(user: UserInterface): string {
-  if (user.name && user.lastname) return `${user.name} ${user.lastname}`
-  return user.email
+  if (user.name && user.lastname) return validator.unescape(`${user.name} ${user.lastname}`)
+  return validator.unescape(user.email.split('@').at(0))
 }
