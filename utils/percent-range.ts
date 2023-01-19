028d6ce05515b5adfe45763a3168e49b9195ffbc
Kazerlelutin <b@bouteiller.contact>
2023-01-19 21:23:53 +0100

    fix(speed-bar): again

--- utils/percent-range.ts
+++ utils/percent-range.ts
@@ -1,8 +1,6 @@
 export const percentRange = (min: number, max: number, value: number): number => {
   const result = ((value - min) * 100) / (max - min)
   const percent = isNaN(result) ? 100 : parseFloat(result.toFixed(2))
-  if (percent === 0) return 100
-  if (percent === 100) return 0
   return percent
 }
 
