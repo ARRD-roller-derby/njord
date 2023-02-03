269f4e0c8245ec611856068f27899ffc4bbabc71
Kazerlelutin <b@bouteiller.contact>
2023-02-03 10:17:30 +0100

    feat(poll): add details for admin

---
+++ components/poll/poll-card-vote-details/poll-card-vote-details.tsx
@@ -0,0 +1,68 @@
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import { IPoll } from '../../../types/poll.interface'
+import { UserInterface } from '../../../types/User.interface'
+import userNameRender from '../../../utils/userNameRender'
+import useFetch from '../../_hooks/useFetch'
+import Factory from '../../_layouts/Factory/Factory'
+import styles from './poll-card-vote-details.module.css'
+
+export interface PollCardVoteDetailsProps {
+  poll: IPoll
+}
+
+export interface PollCardVoteDetailsResults {
+  isAdmin: boolean
+  voters: {
+    id: string,
+    vote: string,
+    voter: UserInterface
+  }[]
+  show: boolean
+  setShow: (value: boolean) => void
+  loading: boolean
+}
+
+export const usePollCardVoteDetails = ({ poll }): PollCardVoteDetailsResults => {
+  const
+    [show, setShow] = useState<boolean>(false),
+    { data: session } = useSession(),
+    { data, loading, refetch } = useFetch<{
+      voters: {
+        id: string,
+        vote: string,
+        voter: UserInterface
+      }[]
+    }>('poll/vote_details', { pollId: poll._id });
+
+  useEffect(() => {
+    if (show && data) refetch()
+  }, [show])
+
+  return {
+    voters: data?.voters || [],
+    isAdmin: session?.user.admin,
+    show, setShow,
+    loading
+  }
+}
+
+export const PollCardVoteDetailsView: React.FC<PollCardVoteDetailsProps & PollCardVoteDetailsResults> = ({ voters, show, setShow, isAdmin, loading }) => (
+  isAdmin ? <div className={styles.container}>
+    <div className={styles.header}>
+      <button type='button' onClick={() => setShow(!show)}>{show ? 'Masquer détails' : 'Voir détails'}</button>
+    </div>
+    <div className={styles.body}>
+      {loading && <div>Chargement...</div>}
+      {show && <div className={styles.voters}>
+        {voters.map((vote) => <div key={vote.id} className={styles.voter}>
+          <div className={styles.voterName}>{userNameRender(vote.voter)}</div>
+          <div className={styles.voterVote}>{vote.vote}</div>
+        </div>)}
+      </div>}
+    </div>
+
+  </div> : <></>
+)
+
+export const PollCardVoteDetails = Factory<PollCardVoteDetailsProps, PollCardVoteDetailsResults>(usePollCardVoteDetails, PollCardVoteDetailsView);	
