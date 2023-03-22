b04b396bd0bbabe2e4643a8920143ee451308494
Kazerlelutin <b@bouteiller.contact>
2023-03-22 20:26:00 +0100

    fix date

--- components/_pageRelated/Calendar/useCalendar.tsx
+++ components/_pageRelated/Calendar/useCalendar.tsx
@@ -1,27 +1,29 @@
-import { useState,useEffect, useContext } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import { EventInterface } from '../../../types/Event.interface'
 import useFetch from '../../_hooks/useFetch'
 import { SocketContext } from '../../../stores/socket.store';
 
 export default function useCalendar() {
-  const [between, setBetween] = useState<[Date,Date]>(),
+  const [between, setBetween] = useState<[Date, Date]>(),
     { data: events, refetch } = useFetch<Array<EventInterface>>('events/events', { between }),
     [triggerRefresh] = useContext(SocketContext);
 
-  useEffect(()=>{
-    if(between) refetch()
-  },[between])
+  useEffect(() => {
+    if (between) refetch()
+  }, [between])
 
   useEffect(() => {
-    if(triggerRefresh && triggerRefresh?.type ==='event' ) {
+    if (triggerRefresh && triggerRefresh?.type === 'event') {
       refetch()
     }
   }, [triggerRefresh])
-  
-  return { refetch, events: events?.sort((a:any,b:any)=>{
-    const 
-      hourA = parseInt(a.hourStart.slice(0,2)),
-      hourB = parseInt(b.hourStart.slice(0,2))
-    return hourA -  hourB
-  }), setBetween }
+
+  return {
+    refetch, events: events?.sort((a: any, b: any) => {
+      const
+        hourA = parseInt(a?.hourStart?.slice(0, 2) ?? 0),
+        hourB = parseInt(b?.hourStart?.slice(0, 2) ?? 0)
+      return hourA - hourB
+    }), setBetween
+  }
 }
--- pages/api/events/presence.ts
+++ pages/api/events/presence.ts
@@ -25,17 +25,14 @@ export default async function event(req: NextApiRequest, res: NextApiResponse) {
   }).select('_id cancel attendees start')
 
 
-  const mercredis = trainings.filter((event) => dayjs(event.start).format('d') == '3')
-  const vendredis = trainings.filter((event) => dayjs(event.start).format('d') == '4')
-  const dimanche = trainings.filter((event) => dayjs(event.start).format('d') == '0')
+  const mercredis = trainings.filter((event) => !!dayjs(event.start).format('LLLL').match(/mercredi|wednes/))
+  const vendredis = trainings.filter((event) => !!dayjs(event.start).format('LLLL').match(/vendre|frid/))
+  const dimanche = trainings.filter((event) => !!dayjs(event.start).format('LLLL').match(/sund|dimanche/))
 
   const filterPre = (e: EventInterface) => !!e.attendees.find(ev => ev.userId === userId && ev.isPresent)
   const presenceMerc = mercredis.filter(filterPre)
   const presenceVen = vendredis.filter(filterPre)
   const presenceDim = dimanche.filter(filterPre)
-
-  console.log(presenceVen, presenceDim)
-
 
   const msg = `
   ${user.name} ${user.lastname} : 
